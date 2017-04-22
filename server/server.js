e2db38693f2ae8864dbb87ddb7641c7d8f0afb01
FelanNemo <anna.pieler@gmx.at>
2017-04-22 11:44:07 +0200

    Problems Add Element

--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-/* Mein erster Webserver */
-
-//var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var bp = require('body-parser');
@@ -18,7 +15,6 @@ var files = [
   {req:'/admin', file:'../admin.html'}
 ];
 
-//console.log(files);
 app.use(bp.urlencoded({extended:true}));
 app.use(express.static('../res'));
 
@@ -94,15 +90,69 @@ app.get('/gametype', function (req, res) {
         console.log(games.game[i].gametyp);
         sendData.push(games.game[i].gametyp);
       }
-
       res.send(sendData);
-      //console.log(sendData);
-
-
     } else {
       console.log('Datei nicht gefunden');
       res.writeHead(200,{'Content-Type':'application/json'});
       res.end(JSON.stringify({file: false}));
     }
+  });
+});
+
+app.post('/select', function(req, res){
+  fs.readFile('../res/games.json', function (err, data) {
+    if(!err){
+      var sendData = [];
+
+        var games = JSON.parse(data);
+        console.log('Datei gefunden');
+        //console.log(req.body.gametyp);
+
+        for(var i in games.game){
+          if(games.game[i].gametyp == req.body.gametyp){
+            for(var j in games.game[i].combinations){
+              sendData.push(games.game[i].combinations[j].result);
+            }
+          }
+        }
+        res.send(sendData);
+        //console.log(sendData);
+    } else {
+
+      }
+  });
+});
+
+app.post('/addEle', function(req, res) {
+  //console.log(req.body);
+
+  var input = req.body;
+
+  fs.readFile('../res/games.json', function (err, data) {
+    if(!err){
+        var games = JSON.parse(data);
+        var g = games.game.gametyp;
+        console.log(g);
+        console.log(input.gametyp);
+
+        if(g == input){
+          games.game.combinations.push(input.result, input.elements)
+
+          console.log(games.game.combinations);
+        }
+
+        //games.game.push(req.body);
+        //fs.writeFile('games.json', JSON.stringify(games));
+        console.log('Datei gefunden');
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({saved: true}))
+    } else {
+          var game = {game:[]};
+          game.game.push(req.body);
+          fs.writeFile('../savegame.json', JSON.stringify(game));
+          console.log('Datei erstellt');
+          res.writeHead(200,{'Content-Type':'application/json'});
+          res.end(JSON.stringify({saved: true}));
+      }
   });
 });
